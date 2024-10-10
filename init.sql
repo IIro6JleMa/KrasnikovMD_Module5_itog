a84c40ac70cb0882f8f8cadafd04437678c4904d
IIro6JleMa <maks.krasnikov.76@bk.ru>
2024-10-10 16:03:24 +0000

    Добавил плейбук для бота и настройки репликации

---
+++ init.sql
@@ -0,0 +1,20 @@
+-- Создание таблицы для email-адресов
+CREATE TABLE IF NOT EXISTS emails (
+    email_id SERIAL PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL
+);
+
+-- Создание таблицы для номеров телефонов
+CREATE TABLE IF NOT EXISTS phones (
+    phone_id SERIAL PRIMARY KEY,
+    phone_number VARCHAR(255) UNIQUE NOT NULL
+);
+
+-- Вставка тестовых данных
+INSERT INTO emails (email) VALUES
+('test1@example.com'),
+('test2@example.com');
+
+INSERT INTO phones (phone_number) VALUES
+('1234567890'),
+('0987654321');
